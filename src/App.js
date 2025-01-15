c3c38809fe2345f50ecbb6c4024e5541405a8d16
lisha-cashify <lishatrn0938@ip-192-168-1-129.ap-south-1.compute.internal>
2025-01-15 13:42:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,79 @@
-import { useCallback, useEffect, useState } from 'react';
+
+import React, { useCallback, useEffect, useState } from 'react';
 import { Virtuoso } from 'react-virtuoso';
+import { Container, Row, Col } from 'reactstrap';
+
 import './App.css';
 
 function App() {
+    const BASE_URL = 'https://picsum.photos/v2/list?page=2&limit=100';
 
-  const ACCSESS_KEY = 'ndqgJs6l0qpQsJ7Y_rMFiN93QcviYFOqAg1Difbmxi8'
-  const BASE_URL = `https://api.unsplash.com/photos/random/?client_id=${ACCSESS_KEY}&count=5`
+    const [photos, setPhotos] = useState([]);
 
-  const [photos, setPhotos] = useState([])
+    const getPhotos = useCallback(async () => {
+        const response = await fetch(BASE_URL);
+        const data = await response.json();
+        setPhotos((prevState) => [...prevState, ...data]);
+    }, [setPhotos]);
 
-  const getPhotos = useCallback(async () => {
-    const response = await fetch(BASE_URL)
-    const data = await response.json()
-    setPhotos((prevState)=>[...prevState, ...data])
-  }, [setPhotos])
+    const getOlderPhotos = useCallback(async () => {
 
-  useEffect(() => {
-    getPhotos().catch(console.error)
-  }, [getPhotos])
+        const response = await fetch(BASE_URL);
+        const data = await response.json();
+        setPhotos((prevState) => [...data, ...prevState]);
+
+    }, [setPhotos]);
+
+    useEffect(() => {
+        getPhotos().catch(console.error);
+    }, [getPhotos]);
+
+    return (
+        <Container fluid className="d-flex flex-column align-items-center vh-100">
+
+            <Row className="w-100">
+                <Col>
+                    <h1 className="text-center mb-2">React Infinite Scroll Virtuoso</h1>
+                </Col>
+            </Row>
 
 
 
-  return (
-    <div style={{width: "100vw", height: "90vh", textAlign: "center"}}>
-      <h1>React Inifinite Scroll Virtuoso</h1>
-      <Virtuoso
-        data={photos}
-        endReached={getPhotos}
-        overscan={200}
-        itemContent={(index, photo)=>{
-          return <img key={index} src={photo.urls.small} alt={`Photo ${index}`} />
-        }}
-      />
-    </div>
-  );
+            <Row style={{ width: '60%' }} className="justify-content-center">
+                <Col xs={12} className="p-0">
+                    <div
+                        style={{
+                            height: '60vh',
+                            width: '100%',
+                            overflow: 'auto',
+                            border: '1px solid #ccc',
+                            borderRadius: '8px',
+                            padding: '10px',
+                        }}
+                    >
+                        <Virtuoso
+                            data={photos}
+                            endReached={getPhotos}
+                            startReached={getOlderPhotos}
+                            overscan={5}
+                            itemContent={(index, photo) => (
+                                <img
+                                    key={index}
+                                    src={photo.download_url}
+                                    alt={`Photo ${index}`}
+                                    style={{
+                                        width: '100%',
+                                        marginBottom: '10px',
+                                        borderRadius: '8px',
+                                    }}
+                                />
+                            )}
+                        />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
